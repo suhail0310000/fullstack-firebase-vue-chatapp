9fc833b1660e4f72189fba835db3c60190835e99
Sevde Oguz <sevdeoguz@wlan-dhcp-10-45-212-38.oslomet.no>
2021-09-09 17:02:12 +0200

    mulig å logge ut brukere- implementert access level

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,36 @@
     <nav>
       <router-link to="/"> Home </router-link> |
       <router-link to="/feed"> Feed </router-link> |
-      <router-link to="/register"> Register </router-link> |
-      <router-link to="/sign-in"> Login </router-link> |
+      <span v-if="isLoggedIn"> 
+        <button @click="signOut"> Logout </button> 
+      </span>
+      <span v-else>
+        <router-link to="/register"> Register </router-link> |
+        <router-link to="/sign-in"> Login </router-link>
+      </span>
     </nav>
     <router-view />
   </div>
 </template>
-
+<script setup>
+import { ref } from 'vue' // used for conditional rendering
+import firebase from 'firebase'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const isLoggedIn = ref(true)
+// runs after firebase is initialized
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      isLoggedIn.value = true // if we have a user
+    } else {
+      isLoggedIn.value = false // if we do not
+    }
+})
+const signOut = () => {
+  firebase.auth().signOut()
+  router.push('/')
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -17,18 +40,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
