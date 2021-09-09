d9af2d6e4ec3405d14bb9b90fae5215f3cd9b61a
Suhail0310000 <s341868@oslomet.no>
2021-09-09 15:30:28 +0200

    mulig å logge inn brukere

---
+++ src/views/SignIn.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1> Login to Your Account </h1>
+    <p> <input type='text' placeholder="Email" v-model='email'/> </p>
+    <p> <input type='password' placeholder="Password" v-model='password'/> </p>
+    <p> <button @click="signIn"> Submit </button> </p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import firebase from 'firebase/app'
+import { useRouter } from 'vue-router' // import router
+const email = ref('')
+const password = ref('')
+const router = useRouter() // get a reference to our vue router
+const signIn = () => { // we also renamed this method 
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email.value, password.value) // THIS LINE CHANGED
+    .then((data) => {
+      console.log('Successfully logged in!');
+      console.log(data);
+      router.push('/feed') // redirect to the feed
+    })
+    .catch(error => {
+      console.log(error.code)
+      alert(error.message);
+    });
+}
+</script>
