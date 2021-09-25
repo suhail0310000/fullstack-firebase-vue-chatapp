87103b22a6153778a2da1d095baa01dbc4bb68f7
Suhail0310000 <s341868@oslomet.no>
2021-09-25 15:46:03 +0200

    kan sende melding til ulike brukere, en bug på chatroom ruter

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,42 +1,14 @@
 <template>
-  <div class="mt-4">
-    <h2>Welcome to Vue.js Chat App</h2>
-    <form class="detail-box my-5">
-      <div class="form-group my-3">
-        <h4>Login to Chat</h4>
-        <input
-          type="text"
-          v-model="email"
-          class="form-control mb-4 mt-4"
-          placeholder="Enter your email..."
-        />
-        <input
-          type="password"
-          v-model="password"
-          class="form-control mb-4"
-          placeholder="Enter your password..."
-          v-on:keyup.enter="login"
-        />
-        <!-- <router-link :to="{ path: 'signup' }">
-          <h6 class="mb-3" style="font-weight: 600">Create an account</h6>
-        </router-link> -->
-
-        <button
-          type="button"
-          v-on:click="login"
-          class="btn btn-primary"
-          style="font-weight: 600"
-        >Login</button>
-      </div>
-    </form>
-  </div>
+    <h1> Login to Your Account </h1>
+    <p> <input type='text' placeholder="Email" v-model='email'/> </p>
+    <p> <input type='password' placeholder="Password" v-model='password'/> </p>
+    <p> <button @click="signIn"> Submit </button> </p>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import firebase from 'firebase/app'
 import { useRouter } from 'vue-router' // import router
-
 const email = ref('')
 const password = ref('')
 const message = ref('')
@@ -56,19 +28,3 @@ const signIn = () => { // we also renamed this method
     })
 }
 </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
